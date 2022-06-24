faca1a1aac5e74209790da8662ee8abfbb94a774
Khayman King <Khaymanak@gmail.com>
2022-06-24 12:25:41 -0700

    user can now post to posts

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -4,14 +4,14 @@ const request = require('supertest');
 const app = require('../lib/app');
 const { Post } = require('../lib/models/Posts');
 jest.mock('../lib/services/github');
-
+const agent = request.agent(app);
 describe('post routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
   
   it('should login in a user and check posts', async() => {
-    const agent = request.agent(app);
+    // const agent = request.agent(app);
     const loginRes = await agent
       .get('/api/v1/github/callback?code=42')
       .redirects(1);
@@ -34,6 +34,32 @@ describe('post routes', () => {
     });
     expect(postRes.body).toEqual(expected);
   });
+});
+
+it('should login a user and create a post ', async() => {
+  // const agent =  agent.request(app);
+  const loginRes = await agent
+    .get('/api/v1/github/callback?code=42')
+    .redirects(1);
+
+  expect(loginRes.body).toEqual({
+    id: expect.any(String),
+    username: 'fake_github_user',
+    avatar: 'https://www.placedog.com/200/200',
+    email: 'fake-email@wowowow.com',
+    iat: expect.any(Number),
+    exp: expect.any(Number),
+  });
+  const res =  await agent
+    .post('/api/v1/posts')
+    .send({
+      post: 'Hey man look this is working and its cool and stuff I think?'
+    });
+  expect(res.status).toBe(200);
+  expect(res.body.post)
+    .toEqual('Hey man look this is working and its cool and stuff I think?');
+
+    
 });
 afterAll(() => {
   pool.end();

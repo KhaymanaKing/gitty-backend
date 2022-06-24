be49760156661613e98620e17821310544fefe76
Khayman King <Khaymanak@gmail.com>
2022-06-24 10:45:38 -0700

    started test for logging in and looking at all posts

---
+++ __tests__/posts.test.js
@@ -0,0 +1,30 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+jest.mock('../lib/services/github');
+
+describe('post routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  
+  it('should login in a user and check posts', async() => {
+    const loginRes = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+
+    expect(loginRes.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      avatar: 'https://www.placedog.com/200/200',
+      email: 'fake-email@wowowow.com',
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
+  });
+});
+afterAll(() => {
+  pool.end();
+});
